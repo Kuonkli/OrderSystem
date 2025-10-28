b2aa0a9e7c9e47a845ca007046852dc22ffe20ef
kuonkli <korotkikh1307@outlook.com>
2025-10-28 20:55:31 +0300

    user model fix

--- services/users/internal/models/user.go
+++ services/users/internal/models/user.go
@@ -9,7 +9,7 @@ import (
 type User struct {
 	ID           uuid.UUID      `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
 	CreatedAt    time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt    time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	UpdatedAt    time.Time      `gorm:"default:CURRENT_TIMESTAMP;constraint:OnUpdate:CURRENT_TIMESTAMP" json:"updated_at"`
 	Email        string         `gorm:"uniqueIndex;not null" json:"email"`
 	PasswordHash string         `gorm:"not null" json:"-"`
 	FirstName    string         `gorm:"not null" json:"first_name"`
